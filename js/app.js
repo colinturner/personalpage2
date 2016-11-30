b00ff1cd2935aa734d7e9b6756ed9b581f5b283d
Colin Turner <colinturner92@gmail.com>
2016-11-30 23:06:34 +0000

    Comment JS code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 window.onload = function() {
 
+//replaces "Brief Biography" with "Other things have happened too though, come on.." when clicked
+
 document.getElementById("titleSwitch1").onclick = function() {changeFirst()};
 
 function changeFirst() {
     document.getElementById("titleSwitch1").innerHTML = "Other things have happened too though, come on..";
 }
 
+//same functionality as previous code, but replaces the second subtitle in the About Me section
 
 document.getElementById("titleSwitch2").onclick = function() {changeSecond()};
 
 function changeSecond() {
-    document.getElementById("titleSwitch2").innerHTML = "When not busy enjoying a little cheeky bagel";
+    document.getElementById("titleSwitch2").innerHTML = "When not busy enjoying a little cheeky bagel..";
 }
 
 }
